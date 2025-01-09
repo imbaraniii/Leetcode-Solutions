46fabb666aa790810515054a69170f3b8314a687
thespectacular314 <baranidharans2005@gmail.com>
2025-01-09 23:51:48 +0530

    python

---
+++ 2185.Counting_words_with_a_given_prefix.py
@@ -0,0 +1,21 @@
+# You are given an array of strings words and a string pref.
+
+# Return the number of strings in words that contain pref as a prefix.
+
+# A prefix of a string s is any leading contiguous substring of s.
+
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        count = 0
+        n = len(pref)
+        # O(n)
+        for word in words:
+            if word[ :n] == pref:
+                count += 1
+
+        return count
+
+
+# O(n)
+
+
